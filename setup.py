f9054e21123b67434b69abee1f980c7dcb7e6ebf
dev-prakhar <prakhars1996@gmail.com>
2019-06-14 21:51:02 +0530

    Bumped Version to 1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def requirements():
 
 setuptools.setup(
     name='social-user-info',
-    version='0.8',
+    version='1.0',
     author="Prakkhar Shrivastava",
     author_email="prakhars1996@gmail.com",
     description="A python package that helps getting the user info from social apps",
